05092226fd75e58b7be4cdd01621ce966876a8e6
ovidionavarro <ovidionavarro90@gmail.com>
2024-11-11 03:05:34 -0500

    listo para rutas priv

--- src/HerosApp.js
+++ src/HerosApp.js
@@ -1,15 +1,32 @@
+import { useEffect, useReducer } from "react"
+import { AuthContext } from "./auth/authContext"
 import { AppRouter } from "./routers/AppRouter"
+import { authReducer } from "./auth/authReducer"
 
 
+
+const init=()=>{
+  return JSON.parse(localStorage.getItem('user'))||{logged:false}
+}
 export const HerosApp = () => {
-  return (
   
-    <AppRouter/>
+  const [user,dispatch]=useReducer(authReducer,{},init)
+  
+  useEffect(()=>{
+    if(!user) return;
+    localStorage.setItem('user',JSON.stringify(user))
+  },[user])
+  
+  
+  return (
+    <AuthContext.Provider value={{
+     user,
+     dispatch
+    }}>
+      <AppRouter/>
+    </AuthContext.Provider>
     
-    // <div>
-    //     <h1>Heros App</h1>
-    // </div> 
-    
+
   )
 }
 // hasme una funcion fibonacci
--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../auth/authContext'
+import { types } from '../../types/types'
 
 export const LoginScreen = () => {
-  
+
 
   const navigate=useNavigate()
+  const {user,dispatch}=useContext(AuthContext)
+  console.log(user,dispatch)
 
+  // console.log(context)
   const handleLogin=()=>{
-    navigate('/marvel',{
+    // navigate('/marvel',{
+    //   replace:true
+    // })
+    const action={
+      type:types.login,
+      payload:{
+        name:'ovidio',
+      }
+    }
+    dispatch(action)
+     navigate('/marvel',{
       replace:true
     })
   }
+  
+  
+
   
   return (
 
